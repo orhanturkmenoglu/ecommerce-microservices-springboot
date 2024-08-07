80666ad0e585491ae8b2f19aac7bb446d3970bad
ORHAN TÜRKMENOĞLU <orhantrkmn749@gmail.com>
2024-08-07 14:19:43 +0300

    corrections made.

---
+++ payment-service/src/test/java/com/example/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
